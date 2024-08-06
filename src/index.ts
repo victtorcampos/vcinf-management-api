8be09c2b5875ada13513726607e10d4ea8dd41f9
victorcampos <victor-campos@outlook.com>
2024-08-06 00:00:37 -0400

    Trabalho feito em casa

--- src/index.ts
+++ src/index.ts
@@ -10,5 +10,7 @@ const resolvers = [ContadorResolvers, UserResolvers, EmpresaResolvers]
 const server = new ApolloServer({ typeDefs, resolvers, context: ({ req }) => { return { prisma, req, }; }, });
 
 server.listen().then(({ url }) => {
+
     console.log(`🚀 Server ready at ${url}`);
+    
 });
